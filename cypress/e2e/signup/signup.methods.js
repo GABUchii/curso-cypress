c43ce13c7aaad743d913887f88301f3244eb6a35
Gabriela Cressa <cressagabriela@gmail.com>
2024-05-27 20:18:01 +0200

    creacion de pagina de signup

---
+++ cypress/e2e/signup/signup.methods.js
@@ -0,0 +1,20 @@
+import { SignupElements } from "./signup.elements";
+
+export class SignupMethods{
+    static insertUserName(username){
+        SignupElements.textBoxes.username.invoke('val',username)
+    }
+
+    static insertPassword(password){
+        SignupElements.textBoxes.password.invoke('val',password)
+    }
+
+    static clickOnSignuoButton(){
+        SignupElements.buttons.singup.click();
+    }
+    static singup(username,password){
+        this.insertUserName(username);
+        this.insertPassword(password);
+        this.clickOnSignuoButton();
+    }
+}
